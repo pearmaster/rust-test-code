2e5aa938ae8279a3ebdef0d49fc18c41e7bf64cc
Jacob Brunson <github@jacobbrunson.com>
2022-07-27 04:35:11 +0000

    mqtt connection object

--- stinger/client/examples/hello.rs
+++ stinger/client/examples/hello.rs
@@ -2,8 +2,6 @@
 use connection::Connection;
 
 fn main() {
-    let mut con = Connection {
-        j: 1
-    };
-    con.publish_one(String::from("Hello world"));
+    let mut con = Connection::new(String::from("tcp://localhost:1883"));
+    con.publish_one(String::from("test/3"), String::from("Hello world stuff"));
 }
--- stinger/connection/src/lib.rs
+++ stinger/connection/src/lib.rs
@@ -5,38 +5,49 @@ use std::time::Duration;
 extern crate paho_mqtt as mqtt;
 
 pub struct Connection {
-    pub j: u32
+    pub cli: mqtt::AsyncClient
 }
 
 impl Connection {
-    pub fn publish_one(&mut self, message: String) {
-            // Create a client & define connect options
-            let cli = mqtt::AsyncClient::new("tcp://localhost:1883").unwrap_or_else(|err| {
-                println!("Error creating the client: {:?}", err);
-                process::exit(1);
-            });
+
+    pub fn new(url: String) -> Connection {
+        let cli = mqtt::AsyncClient::new(url).unwrap_or_else(|err| {
+            println!("Error creating the client: {:?}", err);
+            process::exit(1);
+        });
+
+        let conn_opts = mqtt::ConnectOptionsBuilder::new()
+        .keep_alive_interval(Duration::from_secs(20))
+        .clean_session(true)
+        .finalize();
+
+        // Connect and wait for it to complete or fail
+        if let Err(e) = cli.connect(conn_opts).wait() {
+            println!("Unable to connect:\n\t{:?}", e);
+            process::exit(1);
+        }
+
+        Connection { cli: cli }
+    }
+
+    pub fn publish_one(&mut self, topic: String, message: String) {
         
-            let conn_opts = mqtt::ConnectOptionsBuilder::new()
-                .keep_alive_interval(Duration::from_secs(20))
-                .clean_session(true)
-                .finalize();
-        
-            // Connect and wait for it to complete or fail
-            if let Err(e) = cli.connect(conn_opts).wait() {
-                println!("Unable to connect:\n\t{:?}", e);
-                process::exit(1);
-            }
-        
-            // Create a message and publish it
-            let msg = mqtt::Message::new("test", message, 1);
-            let tok = cli.publish(msg);
-        
-            if let Err(e) = tok.wait() {
-                println!("Error sending message: {:?}", e);
-            }
-        
-            // Disconnect from the broker
-            let tok = cli.disconnect(None);
-            tok.wait().unwrap();
+
+    
+        // Create a message and publish it
+        let msg = mqtt::Message::new(topic, message, 1);
+        let tok = self.cli.publish(msg);
+    
+        if let Err(e) = tok.wait() {
+            println!("Error sending message: {:?}", e);
+        }
+    }
+}
+
+impl Drop for Connection {
+    fn drop(&mut self) {
+        // Disconnect from the broker
+        let tok = self.cli.disconnect(None);
+        tok.wait().unwrap();
     }
 }
