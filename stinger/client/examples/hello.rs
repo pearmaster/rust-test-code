d87b4d740657349bca6052c1bbd39144b04d7106
Jacob Brunson <github@jacobbrunson.com>
2022-07-28 04:11:36 +0000

    WIP functions and closures

--- stinger/client/examples/hello.rs
+++ stinger/client/examples/hello.rs
@@ -1,7 +1,16 @@
 
+use std::{thread, time::Duration};
+
 use connection::Connection;
 
 fn main() {
     let mut con = Connection::new(String::from("tcp://localhost:1883"));
-    con.publish_one(String::from("test/3"), String::from("Hello world stuff"));
+    con.publish(String::from("test/5"), String::from("Hello world stuff"));
+
+    con.subscribe(String::from("hello/world"), 1);
+
+    loop {
+        thread::sleep(Duration::from_millis(1000));
+    }
+    // Ctrl-C to stop
 }
--- stinger/connection/src/lib.rs
+++ stinger/connection/src/lib.rs
@@ -1,11 +1,17 @@
 
 use std::process;
 use std::time::Duration;
+use std::thread;
 
 extern crate paho_mqtt as mqtt;
 
+struct Subscription {
+    topic: String
+}
+
 pub struct Connection {
-    pub cli: mqtt::AsyncClient
+    pub cli: mqtt::AsyncClient,
+    //msg_cb: dyn FnMut(&mqtt::AsyncClient, Option<mqtt::Message>),
 }
 
 impl Connection {
@@ -27,13 +33,32 @@ impl Connection {
             process::exit(1);
         }
 
-        Connection { cli: cli }
+        // Set a closure to be called whenever the client loses the connection.
+        // It will attempt to reconnect, and set up function callbacks to keep
+        // retrying until the connection is re-established.
+        cli.set_connection_lost_callback(|cli: &mqtt::AsyncClient| {
+            println!("Connection lost. Attempting reconnect.");
+            thread::sleep(Duration::from_millis(2500));
+            cli.reconnect();
+        });
+
+        let cb = |_cli, msg: Option<mqtt::Message>| {
+            if let Some(msg) = msg {
+                let topic = msg.topic();
+                let payload_str = msg.payload_str();
+                println!("{} - {}", topic, payload_str);
+            }
+        };
+
+        cli.set_message_callback(cb);
+
+        Connection { 
+            cli: cli, 
+        //    msg_cb: cb 
+        }
     }
 
-    pub fn publish_one(&mut self, topic: String, message: String) {
-        
-
-    
+    pub fn publish(&mut self, topic: String, message: String) {
         // Create a message and publish it
         let msg = mqtt::Message::new(topic, message, 1);
         let tok = self.cli.publish(msg);
@@ -41,6 +66,10 @@ impl Connection {
         if let Err(e) = tok.wait() {
             println!("Error sending message: {:?}", e);
         }
+    }
+
+    pub fn subscribe(&mut self, topic: String, qos: i32) {
+        self.cli.subscribe(topic, qos);
     }
 }
 
